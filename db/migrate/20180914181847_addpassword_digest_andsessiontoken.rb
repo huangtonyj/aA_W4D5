477e7916fa6b7e48612212b93eb8d03aeb869051
huangtonyj <huangtonyj91@gmail.com>
2018-09-14 11:22:57 -0700

    Complete user migration

---
+++ db/migrate/20180914181847_addpassword_digest_andsessiontoken.rb
@@ -0,0 +1,9 @@
+class AddpasswordDigestAndsessiontoken < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :password_digest, :string, null: false
+    add_column :users, :session_token, :string, null: false
+    remove_column :users, :password, :string
+
+    add_index :users, :session_token, unique: true
+  end
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint(8)        not null, primary key
+#  username   :string           not null
+#  password   :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+FactoryBot.define do
+  factory :user do
+    
+  end
+end
